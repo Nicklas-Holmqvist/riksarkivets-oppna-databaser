31ceb9d4f62d9c9f5b318c666e08f02efee92da2
Nicklas-Holmqvist <nicklas_holmqvist@outlook.com>
2023-01-19 21:27:40 +0100

    feat: add hover and active page css

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -1,7 +1,11 @@
 import Link from 'next/link';
 import React, { useEffect, useMemo, useState } from 'react';
 import { useRouter } from 'next/router';
+import styled, { css } from 'styled-components';
 
+interface StyledLiProps {
+  activePage: boolean;
+}
 interface PaginationProps {
   totalItems: number;
   itemsPerPage: number;
@@ -14,24 +18,23 @@ const Pagination: React.FC<PaginationProps> = ({
   paginate,
 }) => {
   const router = useRouter();
-  const { page } = router.query;
+  const { query, push, pathname, asPath } = router;
   let pageNumbers = [];
 
-  const [pages, setPages] = useState([]);
   const [start, setStart] = useState(0);
   const [end, setEnd] = useState(7);
 
-  const currentPage: number = Number(page);
-  const minimumPaginationCollapse: number = 5;
-  const collapsedPaginationNumber: number = 3;
-
-  const collapsedHigherCurrentPage = (): number => {
-    return Number(page) + 3;
-  };
+  const currentPage: number = Number(query.page);
 
   for (let i = 1; i <= Math.ceil(totalItems / itemsPerPage); i++) {
     pageNumbers.push(i);
   }
+
+  useEffect(() => {
+    if (asPath === 'page=1') return;
+    push(pathname + '?page=1');
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [totalItems]);
 
   useEffect(() => {
     if (currentPage <= 5) {
@@ -42,25 +45,53 @@ const Pagination: React.FC<PaginationProps> = ({
       setEnd(currentPage + 3);
     }
   }, [currentPage, paginate]);
-  console.log(currentPage, start, end);
 
   const currentPages = pageNumbers.slice(start, end);
 
   return (
     <nav>
-      <ol>
+      <StyledUl>
         {currentPages.map((number: any) => (
-          <li
+          <StyledLi
             key={number}
-            value={Number(page) === number ? number : 0}
             onClick={() => paginate(number)}
+            activePage={Number(query.page) === number}
           >
             <Link href={`/kurhuset-i-ostersund?page=${number}`}>{number}</Link>
-          </li>
+          </StyledLi>
         ))}
-      </ol>
+      </StyledUl>
     </nav>
   );
 };
 
 export default Pagination;
+
+const StyledUl = styled.ul`
+  width: 20rem;
+  display: flex;
+  justify-content: space-between;
+  margin: 0 auto;
+  padding: 1rem 0;
+`;
+
+const StyledLi = styled.li<StyledLiProps>`
+  width: 2rem;
+  list-style-type: none;
+  text-align: center;
+  padding: 0.5rem 1.4rem 0.5rem 1rem;
+  transition: all ease 0.1s;
+  :hover {
+    background-color: white;
+  }
+  ${({ activePage }) =>
+    activePage
+      ? css`
+          background-color: black;
+          color: white;
+          :hover {
+            color: black;
+          }
+        `
+      : undefined}
+`;
