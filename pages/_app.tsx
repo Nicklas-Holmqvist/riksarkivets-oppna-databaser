d6008fdcabea732eabf017e445a8bd6757001585
Nicklas-Holmqvist <nicklas_holmqvist@outlook.com>
2023-01-26 06:14:25 +0100

    refactor: removed imported global css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,6 @@ import { Layout } from '../components/Layout';
 import { createGlobalStyle } from 'styled-components';
 import type { AppProps } from 'next/app';
 
-import '../styles/globals.css';
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
