bd1b314bf39af69313f085b50c7b2a915cd67689
Nicklas-Holmqvist <nicklas_holmqvist@outlook.com>
2023-02-20 19:42:31 +0100

    refactor: changed url in supabase

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  `${process.env.SUPABASE_URL}`,
+  'https://fpgfodvrkxnnelitclfb.supabase.co',
   `${process.env.SUPABASE_API_KEY_TOKEN}`
 );
